2ff9e888340b10a6be8e2fcf7b21d79d24cb764b
tkachenko <olexiy.tkachenko@gmail.com>
2019-11-05 20:55:57 +0200

    update links.sh

--- links.sh
+++ links.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 echo "Installing dotfiles as symbolic links..."
-DIR="$HOME/src/config"
-# ln -f -s "$DIR/.vimrc" "$HOME/.vimrc"
-# ln -f -s "$DIR/.gitconfig" "$HOME/.gitconfig"
+DIR="$HOME/src/dotfiles"
+
 ln -f -s "$DIR/.zshrc" "$HOME/.zshrc"
 ln -f -s "$DIR/init.vim" "$HOME/.config/nvim/init.vim"
-ln -f -s "$DIR/config.fish" "$HOME/.config/fish/config.fish"
+
+# ln -f -s "$DIR/.vimrc" "$HOME/.vimrc"
+# ln -f -s "$DIR/.gitconfig" "$HOME/.gitconfig"
+# ln -f -s "$DIR/config.fish" "$HOME/.config/fish/config.fish"
